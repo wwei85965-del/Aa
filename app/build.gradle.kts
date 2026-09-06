ad5fd6cdb2882f9043a3a9723bc1859ffe5c1fa8
wwei85965-del <wwei85965@gmail.com>
2026-09-06 21:29:44 +0800

    创建 build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.vv.translate"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.vv.translate"
+        minSdk = 24
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    // Jetpack Compose
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
+    implementation("androidx.activity:activity-compose:1.10.0")
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+
+    // Kotlin Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
+
+    // Google ML Kit
+    // 中文 OCR
+    implementation("com.google.mlkit:text-recognition-chinese:16.0.1")
+
+    // 自动识别语言
+    implementation("com.google.mlkit:language-id:17.0.6")
+
+    // 翻译
+    implementation("com.google.mlkit:translate:17.0.3")
+}
